b4c28f7a23d5dad59165be6d3d081efc9d34e70d
jediel <jediel-felipe@hotmail.com>
2021-03-06 11:46:16 -0300

    version 1.0

---
+++ OportunidadeYESLIST.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdlib.h>
+
+void main () {
+    int num1, num2, g1, g2, g3, g4, g5, soma;
+    printf("Digite a quantidade de litro do galão\n");
+    scanf(num1\n);
+    printf ("Digite a quantidade de garrafas\n");
+    scanf(num2\n);
+    if (num2>2)
+    {
+        printf("Digite quantos litros tem a garrafa numero 1?/n");
+        scanf(g1\n);
+        printf("Digite quantos litros tem a garrafa numero 2?/n")
+        scanf(g2\n);
+
+    }
+    
+}
+
